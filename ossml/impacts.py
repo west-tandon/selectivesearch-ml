62840fa9b549e226b6e548dcde6fc381bee89001
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-04-29 12:30:03 -0400

    Use bucket rank

--- ossml/impacts.py
+++ ossml/impacts.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import logging
 import math
+import fastparquet
+
+from fastparquet import write
 from sklearn.ensemble import RandomForestRegressor
-from ossml.utils import Dataset
 from sklearn.externals import joblib
 from sklearn.utils import shuffle
 from sklearn.metrics import mean_squared_error
@@ -12,69 +14,101 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def feature_columns(dataset):
-    return [f.name for f in dataset.query_features] + [f.name for f in dataset.shard_features] + ['BID']
+def run_train(j, out):
+    features = j['impact_features']
+    basename = j['basename']
+    features_basename = features['basename']
 
+    logger.info("Loading data")
 
-def train_payoffs(dataset, n_jobs=-1, n_estimators=20):
+    index = ['query', 'shard']
 
-    clf = RandomForestRegressor(verbose=True, n_jobs=n_jobs, n_estimators=n_estimators)
+    taily_features = fastparquet.ParquetFile('{}.taily'.format(features_basename))\
+        .to_pandas(columns=index + features['taily'])
+    redde_features = fastparquet.ParquetFile('{}.redde'.format(features_basename))\
+        .to_pandas(columns=index + features['redde'])
+    ranks_features = fastparquet.ParquetFile('{}.ranks'.format(features_basename))\
+        .to_pandas(columns=index + features['ranks'])
+    impacts = pd.concat([fastparquet.ParquetFile('{}#{}.impacts'.format(basename, shard)).to_pandas()
+                         for shard in range(j['shards'])])
+    bucket_ranks = fastparquet.ParquetFile('{}.bucketrank-{}'.format(basename, j['buckets']))
 
-    logger.info("Loading dataset")
-    training_data = dataset.load()
+    logger.info("Joining data")
 
-    features = feature_columns(dataset)
-    X = np.array(training_data[features])
-    y = np.array(training_data['payoff'])
+    data = pd.merge(
+        pd.merge(
+            pd.merge(taily_features, redde_features, on=index),
+            ranks_features,
+            on=index
+        ),
+        impacts,
+        on=index
+    )
+    data = pd.merge(data, bucket_ranks, on=['shard', 'bucket'])
 
-    cut = math.floor(len(X) * 0.7)
-    X, y = shuffle(X, y)
-    X_train, y_train = X[:cut], y[:cut]
-    X_test, y_test = X[cut:], y[cut:]
+    logger.info("Pre-processing data")
+
+    clf = RandomForestRegressor(verbose=True, n_jobs=-1, n_estimators=20)
+    feature_names = features['taily'] + features['redde'] + features['ranks'] + ['bucketrank']
+    features = np.array(data[feature_names])
+    labels = np.array(data['impact'])
+
+    cut = math.floor(len(features) * 0.7)
+    features, labels = shuffle(features, labels)
+    features_train, labels_train = features[:cut], labels[:cut]
+    features_test, labels_test = features[cut:], labels[cut:]
 
     logger.info("Training model")
-    clf.fit(X_train, y_train)
+    clf.fit(features_train, labels_train)
 
     logger.info("Evaluating model")
-    y_pred = clf.predict(X_test)
-    err = mean_squared_error(y_test, y_pred)
+    labels_pred = clf.predict(features_test)
+    err = mean_squared_error(labels_test, labels_pred)
     logger.info("MSE = %f", err)
 
     logger.info("Feature scores: %s",
-                str(sorted(zip(map(lambda x: round(x, 4), clf.feature_importances_), features), reverse=True)))
+                str(sorted(zip(map(lambda x: round(x, 4), clf.feature_importances_), feature_names), reverse=True)))
 
     logger.info("Success.")
-    return clf, err
-
-
-def predict_payoffs(dataset, model):
-    test_data = dataset.load()
-    X = np.array(test_data[feature_columns(dataset)])
-    return test_data, pd.DataFrame(model.predict(X))
-
-
-def run_train(j, out):
-    props = Dataset.parse_json(j, 'impact_features')
-    features = props['impact_features']
-    model, err = train_payoffs(Dataset(features['query'], features['shard'], features['bucket'], props['buckets']))
-    joblib.dump(model, out)
+    joblib.dump(clf, out)
 
 
 def run_predict(j, model_path):
-    props = Dataset.parse_json(j, 'impact_features')
-    features = props['impact_features']
-    logger.info("Loading model")
+    features = j['impact_features']
+    basename = j['basename']
+    features_basename = features['basename']
+
+    logger.info("Loading data")
+
+    index = ['query', 'shard']
+
+    taily_features = fastparquet.ParquetFile('{}.taily'.format(features_basename)) \
+        .to_pandas(columns=index + features['taily'])
+    redde_features = fastparquet.ParquetFile('{}.redde'.format(features_basename)) \
+        .to_pandas(columns=index + features['redde'])
+    ranks_features = fastparquet.ParquetFile('{}.ranks'.format(features_basename)) \
+        .to_pandas(columns=index + features['ranks'])
+    bucket_ranks = fastparquet.ParquetFile('{}.bucketrank-{}'.format(basename, j['buckets']))
+
+    logger.info("Joining data")
+
+    features_data = pd.merge(
+        pd.merge(taily_features, redde_features, on=index),
+        ranks_features,
+        on=index
+    )
+    data = pd.merge(features_data, bucket_ranks, on=['shard'])
+    feature_names = features['taily'] + features['redde'] + features['ranks'] + ['bucketrank']
+
     model = joblib.load(model_path)
-    logger.info("Loading dataset")
-    dataset = Dataset(features['query'], features['shard'], features['bucket'], props['buckets'])
-    logger.info("Making predictions")
-    X, y = predict_payoffs(dataset, model)
-    X['payoff'] = y
-    basename = props['basename']
+    predictions = pd.DataFrame(model.predict(np.array(data[feature_names])))
+    data['impact'] = predictions
+
     logger.info("Storing predictions")
-    for shard, shard_group in X.groupby('SID'):
-        for bucket, bucket_group in shard_group.groupby('BID'):
-            with open("{0}#{1}#{2}.payoff".format(basename, shard, bucket), 'w') as f:
-                for idx, x in bucket_group.sort_values(by='QID').iterrows():
-                    f.write(str(x['payoff']) + "\n")
+    for shard, shard_group in data.groupby('shard'):
+        write('{}#{}.impacts'.format(basename, shard),
+              shard_group[['query', 'shard', 'bucket', 'impact']].sort_values(by=['query', 'bucket']),
+              compression='SNAPPY',
+              write_index=False)
+
     logger.info("Success.")
